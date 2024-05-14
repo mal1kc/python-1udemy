9a3d117a669d12ec17c58067414ab0aebd6b5e9f
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-05-14 20:47:42 +0300

    applied dos2unix

--- 1.seviye/bolum(0-7]/bolum5_problem2_Armstrıng-bulucu.py
+++ 1.seviye/bolum(0-7]/bolum5_problem2_Armstrıng-bulucu.py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem 2
 
 Kullanıcıdan aldığınız bir sayının "Armstrong" sayısı olup olmadığını bulmaya çalışın.
@@ -6,20 +6,21 @@ Kullanıcıdan aldığınız bir sayının "Armstrong" sayısı olup olmadığı
 Örnek olarak, Bir sayı eğer 4 basamaklı ise ve oluşturan rakamlardan herbirinin 4. kuvvetinin toplamı( 3 basamaklı sayılar için 3.kuvveti ) o sayıya eşitse bu sayıya "Armstrong" sayısı denir.
 
 Örnek olarak : 1634 = 1^4 + 6^4 + 3^4 + 4^4
-'''
+"""
 
 sayi = int(input("Sayi : "))
 basamak_sayisi = len(str(sayi))
 basamak = 0
 toplam = 0
 gecici_sayi = sayi
-while (gecici_sayi > 0):
+while gecici_sayi > 0:
     basamak = gecici_sayi % 10
 
-    toplam += (basamak ** basamak_sayisi)
+    toplam += basamak**basamak_sayisi
 
     gecici_sayi //= 10
 if sayi == toplam:
     print("Sayi armstrong sayisidir .")
 else:
     print("Sayi armstrong sayisi değildir . ")
+
